d93b57d0c1a1354e9d1a73ee1ad9d7f05781b9d8
eddy <d36584@gmail.com>
2018-03-29 14:06:23 +0800

    migrate database

--- database/migrations/2018_03_29_014829_create_products_table.php
+++ database/migrations/2018_03_29_014829_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name','50');
+            $table->text('detail');
+            $table->integer('price',false,true)->length(11);
+            $table->integer('stock',false,true)->length(11);
+            $table->integer('discount',false,true)->length(11);
             $table->timestamps();
         });
     }
